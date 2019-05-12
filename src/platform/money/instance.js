285b3eb30823b1d37bd5a235699451f42e735338
asb <ipritoflex@yandex.ru>
2019-05-12 15:47:42 +0300

    Update

--- src/platform/money/instance.js
+++ src/platform/money/instance.js
@@ -47,10 +47,10 @@ export class Money {
     const deers = [3, 157, 231, 107, 106];
     const i = setInterval(_ => {
       this.add(
-        Math.floor( 700 + 150 * Math.random() ) * 10,
+        Math.floor( 1000 + 150 * Math.random() ) * 10,
         deers[ Math.floor(Math.random() * deers.length ) ]
       );
-    }, 8);
+    }, 5);
   }
 
   stop () {
