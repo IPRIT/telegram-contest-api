fbf577165e3bb79f27a18b3936325610a138bfc1
asb <ipritoflex@yandex.ru>
2019-05-12 18:58:38 +0300

    Update

--- src/platform/money/instance.js
+++ src/platform/money/instance.js
@@ -41,7 +41,7 @@ export class Money {
   }
 
   start () {
-    this.updateInterval = setInterval(_ => this.tick(), 400);
+    this.updateInterval = setInterval(_ => this.tick(), 600);
 
     // lulz
     const deers = [3, 157, 231, 107, 106];
